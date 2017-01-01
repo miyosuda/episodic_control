c37bf2f544d810086a44731c607616d0634c3710
Kosuke Miyoshi <miyosuda@gmail.com>
2017-01-02 00:05:08 +0900

    fix random seed for projection

--- ec/projection.py
+++ ec/projection.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+RANDOM_SEED = 1
 
 class Projection(object):
-  # TODO: 乱数を固定にしないとダメか
   def __init__(self, observation_dim, state_dim):
     # Random projection
-    self._matrix_projection = np.random.randn(state_dim,
-                                              observation_dim).astype(np.float32)
+    random_state = np.random.RandomState(RANDOM_SEED)
+    self._matrix_projection = random_state.randn(state_dim,
+                                                 observation_dim).astype(np.float32)
 
   def project(self, observation):
     state = np.dot(self._matrix_projection, observation.flatten())
